0f0bb2c1b26c145072163d1a8387b74522fe8df9
cvm1912 <rajshivam1912@gmail.com>
2026-05-09 17:54:49 +0530

    added(repository)

---
+++ src/repositories/crud-repository.js
@@ -0,0 +1,59 @@
+const { loggerConfig } = require('../config');
+
+class CrudRepository {
+    constructor(model) {
+        this.model = model;
+    }
+
+    async create(data) {
+        try {
+            const response = await this.model.create(data);
+            return response;
+        } catch (err) {
+            loggerConfig.error(err);
+            throw err;
+        }
+    }
+
+    async getAll() {
+        try {
+            const response = await this.model.findAll();
+            return response;
+        } catch (err) {
+            loggerConfig.error(err);
+            throw err;
+        }
+    }
+
+    async get(id) {
+        try {
+            const response = await this.model.findByPk(id);
+            return response;
+        } catch (err) {
+            loggerConfig.error(err);
+            throw err;
+        }
+    }
+
+    async update(id, data) {
+        try {
+            const response = await this.model.update(data, { where: { id } });
+            return response;
+        } catch (err) {
+            loggerConfig.error(err);
+            throw err;
+        }
+    }
+
+    async destroy(id) {
+        try {
+            const response = await this.model.destroy({ where: { id } });
+            return response;
+        } catch (err) {
+            loggerConfig.error(err);
+            throw err;
+        }
+    }
+}
+
+module.exports = CrudRepository;
