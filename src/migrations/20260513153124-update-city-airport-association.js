b5a04973d4baacc67475c281d2a489f84e488971
cvm1912 <rajshivam1912@gmail.com>
2026-05-13 21:24:11 +0530

    added association

---
+++ src/migrations/20260513153124-update-city-airport-association.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addConstraint('Airports', {
+      fields: ['cityId'],
+      type: 'foreign key',
+      name: 'fk_airports_cityId',
+      references: {
+        table: 'Cities',
+        field: 'id'
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE'
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeConstraint('Airports', 'fk_airports_cityId');
+  }
+};
